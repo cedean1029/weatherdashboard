ec6a2070e8bd992f9f5015672a3ad30ab3a01c0c
cedean1029 <cedean1029@gmail.com>
2022-02-07 10:32:08 -0500

    updates to js to pull weather information

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,11 @@ var cityInputEl = document.querySelector("#cityname");
 
 
 var getLonLat = function (city) {
-    //format the weather api
+    //format the city api
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + weatherApiKey;
     console.log(apiUrl);
 
-    //fetch data
+    //fetch lon and lat from first api call
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
@@ -17,8 +17,19 @@ var getLonLat = function (city) {
             console.log(latCoord)
             var lonCoord = data.coord.lon;
             console.log(lonCoord);
+
+            //format weather api
+            var weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latCoord + "&lon=" + lonCoord + "&exclude=alerts&units=standard&appid=" + weatherApiKey;
+            console.log(weatherUrl);
+
+            //fetch weather data
+            fetch(weatherUrl)
+                .then(response => response.json())
+                .then(weather => {
+                    console.log(weather)
+                });
         });
-        
+
 
 };
 
